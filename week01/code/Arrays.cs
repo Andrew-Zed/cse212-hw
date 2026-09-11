ea1ac59d1431d39f5f0b436fc52c6459a75a216a
Andrew Awadike <andrew.awadyke@gmail.com>
2026-09-12 00:05:36 +0100

    Implement right rotation for integer lists

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -12,8 +12,19 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+        double[] multpiles = new double[length]; // creating an array of doubles with the size of length to hold the multiples of the number
 
-        return []; // replace this return statement with your own
+        for (int i = 1; i <= length; i++)
+        {
+            // Calculate the multiple of the number and store it in the array
+            // The multiple is calculated as number * (i + 1)
+
+            multpiles[i - 1] = number * i; // Store the calculated multiple in the array at index i - 1 
+
+        }
+
+
+        return multpiles; // return the new array of multiples
     }
 
     /// <summary>
@@ -29,5 +40,13 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+
+        for (int i = 0; i < amount; i++)
+        {
+            int last = data[data.Count - 1]; // Store the last element of the list in a variable
+            data.RemoveAt(data.Count - 1); // Remove the last element from the list
+            data.Insert(0, last); // Insert the stored last element at the beginning of the list
+        }
+
     }
 }
